3c05f362fbd56ccfcdab201ea06c20aa1fcd991d
sachu0dev <sushil.dev.in@gmail.com>
2024-03-17 13:48:34 +0530

    1.8

--- 1.4/src/components/Header.jsx
+++ 1.4/src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
+const loggedInUser = () =>{
+  return false;
+}
 export const  Title= ()=> {
   return (<a href="/" className="logo">
     Butter & Nan
   </a>);
 }
 export const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return <div className="header ">
     <Title />
     <div className="nav-items">
@@ -12,7 +18,9 @@ export const Header = () => {
         <li><i className="fa-solid fa-gift"></i> Offers</li>
         <li><i className="fa-solid fa-question"></i> Help</li>
         <li><i className="fa-solid fa-cart-shopping"></i> Cart</li>
+        {isLoggedIn ? <button className="sign-btn" onClick={()=> setIsLoggedIn(false)}><i class="fa-solid fa-user"></i> LogOut</button> : <button className="sign-btn" onClick={()=> setIsLoggedIn(true)}><i class="fa-solid fa-user"></i> Login</button>}
       </ul>
+      
     </div>
   </div>
 } 
