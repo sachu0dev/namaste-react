975bca85b4cc62d139e6b0d91bdfbd8cd4a24839
sachu0dev <sushil.dev.in@gmail.com>
2024-03-21 12:53:35 +0530

    1.10

--- 1.4/src/components/About.jsx
+++ 1.4/src/components/About.jsx
@@ -1,15 +1,44 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
-const About = () => {
-  return (
-    <div>
-      <h1>About us Page</h1>
-      <p>
-        {" "}
-        This is the namaste react live Course Chapter - 07 - finding the path
-      </p>
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About us Page</h1>
+//       <p>
+//         {" "}
+//         This is the namaste react live Course Chapter - 07 - finding the path
+//       </p>
 
-      <Outlet />
-    </div>
-  );
-};
+//       <Outlet />
+//     </div>
+//   );
+// };
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: "name",
+      location: "location",
+    };
+    console.log("constructor");
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>About us Page</h1>
+        <p>
+          {" "}
+          This is the namaste react live Course Chapter - 07 - finding the path
+        </p>
+
+        <Outlet />
+      </div>
+    );
+  }
+}
 export default About;
--- 1.4/src/components/Profile.jsx
+++ 1.4/src/components/Profile.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 const Profile = () => {
+  async function getMyinfo() {
+    const data = await fetch("https://api.github.com/users/sachu0dev");
+    const json = await data.json();
+    setName(json.name);
+    setPublicRepos(json.public_repos);
+    setImgUrl(json.avatar_url);
+  }
+  const [name, setName] = useState("name");
+  const [publicRepos, setPublicRepos] = useState("0");
+  const [imgUrl, setImgUrl] = useState("");
+  useEffect(() => {
+    getMyinfo();
+  }, []);
   return (
     <div>
-      <h1>this is profile of sushil</h1>;
+      <h1>My Profile</h1>;<img src={imgUrl}></img>
+      <h2>Name: {name}</h2>
+      <h2>Publice repos: {publicRepos}</h2>
     </div>
   );
 };
