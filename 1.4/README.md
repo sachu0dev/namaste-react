dc845a4f12ba888bf2647d637be9624ce6517b67
sachu0dev <sushil.dev.in@gmail.com>
2024-03-31 13:18:47 +0530

    redux intro

---
+++ 1.4/README.md
@@ -0,0 +1,7 @@
+# Redux toolkit
+- Install @reduxjs/tookit and react-redux
+- Build our store 
+- Connect our store to our app
+- Slice (cartSlice)
+- dispatch(action)
+- Selector
--- 1.4/src/App.jsx
+++ 1.4/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy, useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import appStore from "./Utils/Redux/appStore.js";
 import UserContext from "./Utils/UserContext.js";
 import Body from "./components/Body.jsx";
 import Contact from "./components/Contact.jsx";
@@ -11,9 +13,9 @@ import Profile from "./components/Profile.jsx";
 import RestaurantMenu from "./components/RestaurantMenu.jsx";
 import Shimmer from "./components/Shimmer.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 const Instamart = lazy(() => import("./components/Instamart.jsx"));
 const About = lazy(() => import("./components/About.jsx"));
+
 const AppLayout = () => {
   const [userName, setUserName] = useState();
 
@@ -27,13 +29,15 @@ const AppLayout = () => {
     setUserName(data.name);
   }, []);
   return (
-    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-      <>
-        <Header />
-        <Outlet />
-        <Footer />
-      </>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <>
+          <Header />
+          <Outlet />
+          <Footer />
+        </>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 const appRouter = createBrowserRouter([
